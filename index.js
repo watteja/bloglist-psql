750931d1eca67ea18729a20b926c5c8377bfb167
watteja <mateyast@gmail.com>
2025-03-13 14:48:46 +0100

    Exercise 13.4

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    });
-    for (const blog of blogs) {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
-    }
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
   }
-};
+);
 
-main();
+// Create schema if it doesn't exist
+Blog.sync();
+
+app.get("/api/blogs", async (_req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// parse incoming JSON data
+app.use(express.json());
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log(`Deleting blog ${JSON.stringify(blog, null, 2)}`);
+    await blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
